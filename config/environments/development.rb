03914e83733c10eed1d2cc4e160b8740f2c64a37
Cleiton Corrêa <cleitoncorreas@gmail.com>
2021-07-11 02:09:49 -0300

    Config MailCatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,16 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+
+  config.action_mailer.raise_delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    #Host MailCatcher Docker
+    address: 'mailcatcher',
+    port: 1025
+  }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.

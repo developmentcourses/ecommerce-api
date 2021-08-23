8c53a4d945e9868dca126f7f68a7d243d7e2e183
Cleiton Corrêa da Silva <cleiton.silva@guide.com.br>
2021-08-23 00:43:39 -0300

    Shared errors

--- spec/requests/admin/v1/categories/unauthenticated_requests_spec.rb
+++ spec/requests/admin/v1/categories/unauthenticated_requests_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Admin::V1::Categories without authentication", type: :request do
 
-    context "GET /cateogries" do
+    context "GET /categories" do
         let(:url) { "/admin/v1/categories" }
         let!(:categories) { create_list(:category, 5) }
 
